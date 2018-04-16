ffa3fd4a268d01a47e26479053bb80eff2425099
zhanghonged <zhanghonged@126.com>
2018-04-16 22:22:11 +0800

    v1.0.0.1

--- VueShop/urls.py
+++ VueShop/urls.py
@@ -19,7 +19,12 @@ import xadmin
 from VueShop.settings import MEDIA_ROOT
 from django.views.static import serve
 
+from goods.views_base import GoodsListView
+
 urlpatterns = [
     url(r'xadmin/', xadmin.site.urls),
     url(r'^media/(?P<path>.*)$', serve, {"document_root":MEDIA_ROOT}),
+
+    #商品列表页
+    url(r'goods/$', GoodsListView.as_view(), name="goods-list")
 ]
