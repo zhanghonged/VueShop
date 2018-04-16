584c020a8db2e235bc0e5a9b55c4024febd70b46
zhanghonged <zhanghonged@126.com>
2018-04-16 21:45:00 +0800

    商品数据准备

--- VueShop/urls.py
+++ VueShop/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 # from django.contrib import admin
 from django.conf.urls import include, url
 import xadmin
+from VueShop.settings import MEDIA_ROOT
+from django.views.static import serve
 
 urlpatterns = [
-    url('xadmin/', xadmin.site.urls),
+    url(r'xadmin/', xadmin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve, {"document_root":MEDIA_ROOT}),
 ]
--- apps/goods/models.py
+++ apps/goods/models.py
@@ -15,7 +15,7 @@ class GoodsCategory(models.Model):
     name = models.CharField(default="", max_length=30, verbose_name="类别名", help_text="类别名")
     code = models.CharField(default="", max_length=30, verbose_name="类别code", help_text="类别code")
     desc = models.TextField(default="", verbose_name="类别描述", help_text="类别描述")
-    category_type = models.CharField(max_length=100, choices=CATEGORY_TYPE, verbose_name="类目级别", help_text="类目级别")
+    category_type = models.IntegerField(choices=CATEGORY_TYPE, verbose_name="类目级别", help_text="类目级别")
     parent_category = models.ForeignKey("self", null=True, blank=True, verbose_name="父类目级别", help_text="父目录", related_name="sub_cat")
     is_tab = models.BooleanField(default=False, verbose_name="是否导航", help_text="是否导航")
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
@@ -63,7 +63,7 @@ class Goods(models.Model):
     goods_desc = UEditorField(verbose_name="内容", imagePath="goods/images/", width=1000, height=300,
                               help_text="",filePath="goods/files/",default="")
     ship_free = models.BooleanField(default=True, verbose_name="是否承担运费")
-    goods_front_image = models.ImageField(upload_to="", null=True, blank=True, verbose_name="封面图")
+    goods_front_image = models.ImageField(upload_to="goods/images/", null=True, blank=True, verbose_name="封面图")
     is_new = models.BooleanField(default=False, verbose_name="是否新品")
     is_hot = models.BooleanField(default=False, verbose_name="是否热销")
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
@@ -80,8 +80,7 @@ class GoodsImage(models.Model):
     商品轮播图
     """
     goods = models.ForeignKey(Goods,verbose_name="商品", related_name="image")
-    image = models.ImageField(upload_to="", verbose_name="图片", null=True, blank=True)
-    image_url = models.CharField(max_length=300, null=True, blank=True, verbose_name="图片url")
+    image = models.ImageField(upload_to="goods/images/", verbose_name="图片", null=True, blank=True)
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     class Meta:
